982060da8cd77af1ff8b9395bbdb02a49310af46
Ronald Benitez <ronald05101998@gmail.com>
2022-10-26 22:40:11 -0500

    Ejercicio practico Expo-E5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }, 
+  {
+    path: 'nosotros',
+    component: NosotrosComponent
+  }, 
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NosotrosComponent,
+    ContactanosComponent,
+    NabvarComponent,
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
